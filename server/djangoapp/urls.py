a57af1fb06c14aea96ee48b518f89526908b6a61
Saravanan <saravanan_kuppusamy@yahoo.com>
2026-01-23 16:01:25 -0500

    fixed lint errors

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     # path for get_cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
     # path for get_dealers view
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>',

0f114ae2f019e8d4594d48949e1b2bbd73fd384c
Saravanan <saravanan_kuppusamy@yahoo.com>
2026-01-23 15:08:38 -0500

    fixed lint errors

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,5 +1,6 @@
 from .models import CarMake, CarModel
 
+
 def initiate():
     car_make_data = [
         {"name":"NISSAN", "description":"Great cars. Japanese technology"},
@@ -11,7 +12,7 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-            car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+        car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
 
 
     # Create CarModel instances with the corresponding CarMake instances
@@ -35,4 +36,4 @@ def initiate():
     ]
 
     for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+        CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
